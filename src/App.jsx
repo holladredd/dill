0ae8ffcbaeba1ebb3e71de1346fee56ad73ed2d7
olamide folayan <84618342+holladredd@users.noreply.github.com>
2024-01-22 15:29:38 +0000

    includes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Typography from '@mui/material/Typography'
 
 function App() {
   const [count, setCount] = useState(0)
+  const name = 'shola and tunde'
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Typography variant="h1" color="initial"><h1>{name}</h1></Typography>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
