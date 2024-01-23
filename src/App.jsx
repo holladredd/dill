8f6e569987783486b7ca7d9570190774637a27cc
olamide folayan <84618342+holladredd@users.noreply.github.com>
2024-01-23 09:48:20 +0000

    commiting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Typography from '@mui/material/Typography'
+import { Add } from '@mui/icons-material'
+import Button from '@mui/material/Button'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const name = 'shola and tunde'
+
+  
 
   return (
-    <>
-      <div className="App">
-        <Typography variant="h1" color="initial"><h1>{name}</h1></Typography>
-      </div>
-    </>
+    <div>
+      <Button variant="contained" color="primary" startIcon={<Add />}>
+        Hello World
+      </Button>
+    </div>
   )
 }
 
